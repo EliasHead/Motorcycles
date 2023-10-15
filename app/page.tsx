e65eaf4f157ebf4f950c76f5c762ef0d4797449a
ELIASHEAD <eliashead.costa@gmail.com>
2023-10-15 20:02:36 -0300

    config prisma ORM e teste de conexão

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <div className="m-12">
+      <Button variant="destructive">Home</Button>
+    </div>
+  )
 }
